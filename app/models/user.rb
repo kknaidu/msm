1b53eb2ac8c8b7fd4d667139fedb8f76bb3afc41
jagadeesh <k.jagadeesh707@gmail.com>
2014-10-04 11:36:11 +0530

    Added devise gem to project

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,13 @@ class User
   field :current_sign_in_ip, type: String
   field :last_sign_in_ip,    type: String
 
+  class << self
+  def serialize_from_session(key, salt)
+    record = to_adapter.get(key[0]["$oid"])
+    record if record && record.authenticatable_salt == salt
+  end
+  end
+
   ## Confirmable
   # field :confirmation_token,   type: String
   # field :confirmed_at,         type: Time
